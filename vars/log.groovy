a146a74a9b3e633ce629535baad0104f829f399a
muthoju <101058622+muthoju@users.noreply.github.com>
2023-08-22 15:56:33 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,3 +1,4 @@
 def call() {
-    echo "Hello from my custom step!"
+    // ANSI color codes: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    echo "\u001B[33mHello from my custom step in yellow!\u001B[0m"  // Yellow color
 }
