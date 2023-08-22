9a64078d046a62e90e171361fabc834164d01d86
muthoju <101058622+muthoju@users.noreply.github.com>
2023-08-22 18:50:02 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,20 +1,56 @@
-package org.jenkinsci.plugins.mylibrary
+import groovy.json.JsonOutput;
 
-def call(logPath) {
-    pipeline {
-        agent any
-        options {
-            ansiColor('xterm')
-        }
-        stages {
-            stage('Build') {
-                steps {
-                    script {
-                        echo "Log path: ${logPath}"
-                        // Rest of your pipeline steps...
-                    }
-                }
-            }
+// Color
+def red(message) {
+    echo "\033[1;31m${message} \033[0m"
+}
+
+def green(message) {
+    echo "\033[1;32m${message} \033[0m"
+}
+
+def blue(message) {
+    echo "\033[1;34m${message} \033[0m"
+}
+
+def yellow(message) {
+    echo "\033[1;33m${message} \033[0m"
+}
+
+// Log
+def title(message) {
+    blue "[INFO] >>> ${message}"
+}
+
+def info(message) {
+    green "[INFO] ${message}"
+}
+
+def warn(message) {
+    yellow "[WARN] ${message}"
+}
+
+def error(message) {
+    red "[ERROR] ${message}"
+}
+
+def prettyPrint(data) {
+    def json = JsonOutput.toJson(data)
+    def pretty = JsonOutput.prettyPrint(json)
+    println (pretty)
+}
+
+def colorizeMavenOutput(output) {
+    output.split('\n').each { line ->
+        if (line.startsWith('[INFO]')) {
+            info(line)
+        } else if (line.startsWith('[WARN]') || line.startsWith('[WARNING]')) {
+            warn(line)
+        } else {
+            echo(line)
         }
     }
 }
+
+// Apply color formatting to Maven build output
+colorizeMavenOutput(currentBuild.rawBuild.getLog(1000))
