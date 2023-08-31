6f8f0913e2c123911a830209906bad4d760f3edb
muthoju <101058622+muthoju@users.noreply.github.com>
2023-09-01 00:27:32 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,56 +1,13 @@
-import groovy.json.JsonOutput;
-
-// Color
-def red(message) {
-    echo "\033[1;31m${message} \033[0m"
-}
-
-def green(message) {
-    echo "\033[1;32m${message} \033[0m"
-}
-
-def blue(message) {
-    echo "\033[1;34m${message} \033[0m"
-}
-
-def yellow(message) {
-    echo "\033[1;33m${message} \033[0m"
-}
-
-// Log
-def title(message) {
-    blue "[INFO] >>> ${message}"
-}
-
-def info(message) {
-    green "[INFO] ${message}"
-}
-
-def warn(message) {
-    yellow "[WARN] ${message}"
-}
-
-def error(message) {
-    red "[ERROR] ${message}"
-}
-
-def prettyPrint(data) {
-    def json = JsonOutput.toJson(data)
-    def pretty = JsonOutput.prettyPrint(json)
-    println (pretty)
-}
-
-def colorizeMavenOutput(output) {
-    output.split('\n').each { line ->
-        if (line.startsWith('[INFO]')) {
-            info(line)
-        } else if (line.startsWith('[WARN]') || line.startsWith('[WARNING]')) {
-            warn(line)
-        } else {
-            echo(line)
-        }
+def colorizeOutput(line) {
+    if (line.startsWith("[INFO]")) {
+        return "\u001B[32m${line}\u001B[0m" // Green color for [INFO]
+    } else if (line.startsWith("[WARNING]")) {
+        return "\u001B[31m${line}\u001B[0m" // Red color for [WARNING]
+    } else if (line.startsWith("[debug]")) {
+        return "\u001B[35m${line}\u001B[0m" // Magenta color for [debug]
+    } else {
+        return line // No color for other lines
     }
 }
 
-// Apply color formatting to Maven build output
-colorizeMavenOutput(currentBuild.rawBuild.getLog(1000))
+return this
