a192cf4d0b83d3cbe916680d7d66c94259db7831
muthoju <101058622+muthoju@users.noreply.github.com>
2023-09-29 21:12:23 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,13 +1,19 @@
-def colorizeOutput(line) {
+// Shared Groovy script: colorizeOutput.groovy
+
+def call(line) {
     if (line.startsWith("[INFO]")) {
-        return "\u001B[32m${line}\u001B[0m" // Green color for [INFO]
+        ansiColor('xterm') {
+            echo "\u001B[32m${line}\u001B[0m" // Green color for [INFO]
+        }
     } else if (line.startsWith("[WARNING]")) {
-        return "\u001B[31m${line}\u001B[0m" // Red color for [WARNING]
+        ansiColor('xterm') {
+            echo "\u001B[31m${line}\u001B[0m" // Red color for [WARNING]
+        }
     } else if (line.startsWith("[debug]")) {
-        return "\u001B[35m${line}\u001B[0m" // Magenta color for [debug]
+        ansiColor('xterm') {
+            echo "\u001B[35m${line}\u001B[0m" // Magenta color for [debug]
+        }
     } else {
-        return line // No color for other lines
+        echo line // No color for other lines
     }
 }
-
-return this
